5e1c4ae1afa1685b49b13e0f3a043c6f044e34a1
King Akidi <onlinemraam@gmail.com>
2022-05-28 01:37:23 +0100

    first commit

---
+++ src/components/HomeComponent.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="jokes-container">
+    <h3>Welcome to Random Jokes</h3>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      jokeApiUrl: "https://v2.jokeapi.dev/joke/Programming",
+    };
+  },
+};
+</script>
+
+<style></style>
